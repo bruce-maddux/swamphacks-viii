70d6c1e57facbf6adbca4d7c44d70da3dd0a59ad
samirziad123 <samirziad32@gmail.com>
2022-01-29 02:40:02 -0500

    implemented maps

--- Frontend/components/map.js
+++ Frontend/components/map.js
@@ -1,14 +1,42 @@
 import React from 'react'
-import { Text, View } from 'react-native';
-import mapStyles from '../StyleSheets/mapStyles'
+import { Platform, StyleSheet, Text, View } from 'react-native';
+import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
+import Geolocation from "react-native-geolocation-service";
 class map extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            latitude: 0,
+            longitude: 0,
+            coordinates: [],
+        };
+    }
+    componentDidMount() {
+
+    }
+
     render()
     {
         return(
-            <View style = {mapStyles.container}>
-                <Text style = {{textAlign:"center"}}>Map</Text>
-            </View>
-        )
+            <MapView
+        style={{ flex: 1 }}
+        provider={PROVIDER_GOOGLE}
+        showsUserLocation
+        region={{
+        latitude: this.state.latitude,
+        longitude: this.state.longitude,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421}}
+        showsUserLocation={true}
+        >
+       <Marker
+           coordinate={{
+               latitude: this.state.latitude,
+               longitude: this.state.longitude,
+           }}>
+       </Marker>
+        </MapView>
+        );
     }
 }
 export default map;
